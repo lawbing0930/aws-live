13f14d966323897410317710fff268386bb880d2
lawbing0930 <139418295+lawbing0930@users.noreply.github.com>
2023-12-20 08:38:21 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cb6oanficzff.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lawbing-employee"
+customregion = "us-east-1"
+
